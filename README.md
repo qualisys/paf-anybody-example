b6539d956c9b24fe6cd07ddac2772be1acbb960d
PROLAN\Andrey.Aksenov <andrey.aksenov@qualisys.se>
2023-11-17 13:49:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Qualisys PAF AnyBody example
+
+This example automatically generates AnyBody projects for IOR, CAST, and Sport full-body marker sets for walking gait analyses using two force plates. 
+It prepares all necessary AnyBody scripts, copies all C3D files, establishes links between them, and also incorporates the weight and height of the subject. The project includes three full-body walking gait examples, each utilizing different marker sets. Once the project (IOR, CAST, or Sport) is generated, it can be run with the AnyBody Modeling software to produce the data. If the PAF AnyBody example is used with data without force plate, the script should be modified to be able to predict forces. This modification applies similarly to lower body analyses, running on a treadmill, etc. The project contains a 'Documentation' folder, which includes guides for IOR, CAST, and Sport marker sets.
+
+## Getting started
+To download this example project to your computer, you can either:
+
+* [Click here](https://github.com/qualisys/paf-anybody-example/archive/refs/heads/main.zip) to download it as a zip file.
+<br>_— or —_
+* Clone this repository to your computer.
+
+## Preparing Qualisys data for AnyBody processing
+### Preparation
+1. Downaload and install AnyBody Modeling System from https://www.anybodytech.com/resources/customer-downloads/ for exisiting customeres or request a trial version from https://www.anybodytech.com/resources/trial-license/
+   Register the license (see the https://www.anybodytech.com/download/installguide-anybody/)
+   - Getting started https://youtu.be/Y4H-8FMJlis with AnyBody videos on YouTube
+   - E-learning https://www.youtube.com/user/anybodytech/videos videos on YouTube
+   - Tutorials in AnyBody’s Help > Tutorials menu
+   - Wiki and user forum on https://anyscript.org/
+2. Open PAF AnyBody Example
+   - Inside, you will find 3 walking gait examples for the IOR, CAST, and Sport marker sets. Open the '01 John Doe' tree, and then select '2023-11-14_IOR'.
+   - Click 'Start Processing.' It generate an IOR folder in the current session containing all the necessary AnyBody scripts for this session.
+   - Locate the '2023-11-14_IOR' folder and right-click to select 'Open folder in explorer.'
+   - You will find a folder named 'IOR' (for the CAST project, this folder would be named 'CAST,' and similarly for the Sport marker set).
+3. Running AnyBody software
+   1. Open the file ..IOR\Subjects\SQ1\Static 1\Main.any using the AnyBody software.
+   2. In the AnyBody software, press 'Load' or F7 to load the model.
+   3. Then, select 'RunParameterIdentification' next to it and press 'Execute.' This process may take approximately 5 minutes to fit the model and markers.
+   4. Afterwards, select 'RunAnalysis' and press 'Execute.' This will perform inverse kinematics, inverse dynamics, and save the file.
+   5. To process your dynamic trials, navigate to ..\IOR\Subjects\SQ1\IOR FB 1\Main.any, press 'Load,' choose 'RunAnalysis,' and then execute.
+   6. Your dynamic trial will be processed, and you can review your data in 'Model View 1 => Chart 1.'
+   7. Follow the same processing procedure for other dynamic trials as described in step 6.
+
+
+
+### Loading issues
+If the model fails to load, you may need to manually specify the installation path with of the model example and modify the script accordingly.
+1. During the installation of AnyBody Modeling, make a note of where the \Documents\AnyBody.7.4.x\AMMR.v2.4.4-Demo folder is located, or find it manually.
+2. Copy the path. In this case, it is C:\Users\UserName\Documents\AnyBody.7.4.x\AMMR.v2.4.4-Demo.
+3. Open the generated IOR project and locate the libdef.any file..
+4. Open it with a text editor, such as Notepad, and replace this line of code:
+#ifndef LIBDEF_ANY
+#include "<ANYBODY_PATH_INSTALLDIR>/AMMR/libdef.any"
+#endif
+
+with your path. In this example, it would be: #include "C:\Users\UserName\Documents\AnyBody.7.4.x/AMMR.v2.4.4-Demo/libdef.any" 
+5. You may also need to modify the main.any files in ..\IOR\Subjects\SQ1\ by replacing the code above with:
+#include "../../../libdef.any"
+
+
+### Video tutorial
+
+
+## Resources for using the Qualisys Project Automation Framework (PAF)
+
+The purpose of the ***Project Automation Framework*** (PAF) is to streamline the motion capture process from data collection to the final report. This repository contains an example project that illustrate how PAF can be used to implement custom automated data collection in [Qualisys Track Manager (QTM)](http://www.qualisys.com/software/qualisys-track-manager/), and how QTM can be connected to a processing engine. 
+
+### PAF Documentation
+
+The full documentation for PAF development is available here: [PAF Documentation](https://github.com/qualisys/paf-documentation).
+
+
+### PAF Examples
+
+Our official examples for various processing engines:
+
+- [Excel](https://github.com/qualisys/paf-excel-example)
+- [Matlab](https://github.com/qualisys/paf-matlab-example)
+- [OpenSim](https://github.com/qualisys/paf-opensim-example)
+- [AnyBody](https://github.com/qualisys/paf-anybody-example)
+- [Python](https://github.com/qualisys/paf-python-example)
+- [Theia Markerless](https://github.com/qualisys/paf-theia-markerless-example)
+- [Theia Markerless Comparison](https://github.com/qualisys/paf-theia-markerless-comparison-example)
+- [Theia Markerless True Hybrid](https://github.com/qualisys/paf-theia-markerless-true-hybrid-example)
+- [Visual3D](https://github.com/qualisys/paf-visual3d-example)
+
+
+_As of QTM version 2.17, the official Qualisys PAF examples can be used without any additional license. Note that some more advanced analysis types require a license for the "PAF Framework Developer kit" (Article number 150300)._
---
+++ Templates/RunProcessingIOR.php
@@ -0,0 +1,180 @@
+<?php
+$work_dir = str_replace('\\', '\\\\', $working_directory);
+
+// Function to recursively copy files and directories
+function recurse_copy($src, $dst, $copy_all = true, $specific_files = [])
+{
+    $dir = opendir($src);
+    @mkdir($dst);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file) && $copy_all) {
+                recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                if ($copy_all || in_array($file, $specific_files)) {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+    }
+    closedir($dir);
+}
+
+// Load parameters.xml file
+$params_xml = simplexml_load_file($working_directory . "parameters.xml");
+$template_directory = "";
+foreach ($params_xml->group as $group) {
+    if ((string)$group['name'] == 'template_directory') {
+        $template_directory = (string)$group->param['second'];
+        break;
+    }
+}
+$temp_dir = str_replace('\\', '\\\\', $template_directory);
+
+// Copy the entire 'IOR' directory from templates to working directory
+$IOR_template_dir = $temp_dir . "Anybody\\IOR";
+$IOR_work_dir = $work_dir . "\\IOR";
+recurse_copy($IOR_template_dir, $IOR_work_dir);
+
+// Copy specific files for Subject SQ1
+$IOR_subjects_sq1_dir = $temp_dir . "Anybody\\IOR\\Subjects\\SQ1";
+$IOR_subjects_sq1_work_dir = $work_dir . "\\IOR\\Subjects\\SQ1";
+@mkdir($IOR_subjects_sq1_work_dir, 0777, true);
+$specific_files = ['SubjectSpecificData.any']; // The specific file(s) to copy
+
+foreach ($specific_files as $specific_file) {
+    if (file_exists($IOR_subjects_sq1_dir . "\\" . $specific_file)) {
+        copy($IOR_subjects_sq1_dir . "\\" . $specific_file, $IOR_subjects_sq1_work_dir . "\\" . $specific_file);
+    }
+}
+
+
+
+// Process dynamic trials from parameters.xml and handle Static 1 separately
+$dynamic_trials = [];
+foreach ($params_xml->group as $group) {
+    if ((string)$group['name'] == 'measurement_guids') {
+        foreach ($group->param as $param) {
+            $trial_name = str_replace('.qtm', '', (string)$param['first']);
+            if (preg_match('/Static 1|IOR FB \d+/', $trial_name)) {
+                // Static 1 will be copied directly, IOR FB trials will be copied from Dynamic 1
+                $dynamic_trials[$trial_name] = $trial_name;
+            }
+        }
+    }
+}
+
+// Function to find and rename the first available static c3d file
+function rename_first_static_c3d($work_dir) {
+    $renamed = false;
+    for ($i = 1; $i <= 4; $i++) {
+        $old_file = $work_dir . "\\Static $i.c3d";
+        if (file_exists($old_file)) {
+            $new_file = $work_dir . "\\Static 1.c3d";
+            if ($i !== 1) { // No need to rename if it's already Static 1
+                rename($old_file, $new_file);
+            }
+            $renamed = true;
+            break;
+        }
+    }
+    return $renamed;
+}
+
+// Find and rename the first available static c3d file
+$static_renamed = rename_first_static_c3d($work_dir);
+
+// Copy all .c3d files to a specific folder in the working directory
+$c3d_files_dir = $work_dir . "\\IOR\\c3d_files";
+@mkdir($c3d_files_dir, 0777, true);
+foreach (glob($work_dir . '\\*.c3d') as $c3d_file) {
+    $file_name = basename($c3d_file);
+    copy($c3d_file, $c3d_files_dir . '\\' . $file_name);
+}
+
+// If a static c3d file was renamed, ensure only the new Static 1.c3d exists in c3d_files directory
+if ($static_renamed) {
+    // Remove any other static c3d files that may have been copied
+    for ($i = 2; $i <= 4; $i++) {
+        $extra_file = $c3d_files_dir . "\\Static $i.c3d";
+        if (file_exists($extra_file)) {
+            unlink($extra_file);
+        }
+    }
+}
+
+foreach ($dynamic_trials as $trial_name) {
+    $source_dir = $trial_name;
+    // Determine if it's a IOR FB trial and not the first one
+    if (strpos($trial_name, 'IOR FB') !== false && $trial_name != 'IOR FB 1') {
+        $source_dir = 'IOR FB 1'; // Use IOR FB 1 as the source for all other IOR FB trials
+    }
+
+    $source_path = $IOR_subjects_sq1_dir . "\\" . $source_dir;
+    $dest_path = $IOR_subjects_sq1_work_dir . "\\" . $trial_name;
+    @mkdir($dest_path, 0777, true);
+    
+    // Use recurse_copy function defined earlier to copy the directory
+    recurse_copy($source_path, $dest_path);
+}
+
+
+
+// Copy all .c3d files to a specific folder in the working directory
+$c3d_files_dir = $work_dir . "\\IOR\\c3d_files";
+@mkdir($c3d_files_dir, 0777, true);
+foreach (glob($work_dir . '\\*.c3d') as $c3d_file) {
+    $file_name = basename($c3d_file);
+    copy($c3d_file, $c3d_files_dir . '\\' . $file_name);
+}
+
+
+// Define the directory containing the session.xml
+$xml_directory = $work_dir; // Replace with the actual directory path if different
+
+// Define the file paths for session.xml and SubjectSpecificData.any
+$session_xml_file = $xml_directory . "\\session.xml";
+$subject_specific_data_file = $xml_directory . "\\IOR\\Subjects\\SQ1\\SubjectSpecificData.any";
+
+// Check if session.xml file exists
+if (file_exists($session_xml_file)) {
+    // Load the XML file with DOMDocument
+    $dom = new DOMDocument();
+    $dom->load($session_xml_file);
+
+    // Access the Height and Weight values
+    $height = $dom->getElementsByTagName('Height')->item(0)->nodeValue;
+    $weight = $dom->getElementsByTagName('Weight')->item(0)->nodeValue;
+
+    // Format the height and weight values
+    $height_formatted = number_format((float)$height, 2, '.', '');
+    $weight_formatted = number_format((float)$weight, 1, '.', '');
+
+    // Check if the values are numeric and not zero
+    if (is_numeric($height) && is_numeric($weight) && $height != 0 && $weight != 0) {
+        // Check if SubjectSpecificData.any file exists
+        if (file_exists($subject_specific_data_file)) {
+            // Read SubjectSpecificData.any file
+            $subject_specific_data_contents = file_get_contents($subject_specific_data_file);
+
+            // Replace BodyMass and BodyHeight with the new values from session.xml
+            // Ensure that only one semicolon is present at the end of each line
+            $subject_specific_data_contents = preg_replace('/(BodyMass\s*=\s*)\d+(\.\d+)?\s*;*/', "BodyMass = {$weight_formatted};", $subject_specific_data_contents);
+            $subject_specific_data_contents = preg_replace('/(BodyHeight\s*=\s*)\d+(\.\d+)?\s*;*/', "BodyHeight = {$height_formatted};", $subject_specific_data_contents);
+
+            // Save the modified contents back to the SubjectSpecificData.any file
+            file_put_contents($subject_specific_data_file, $subject_specific_data_contents);
+        } else {
+            die("Error: The SubjectSpecificData.any file does not exist in the specified directory.");
+        }
+    } else {
+        die("Error: Invalid height or weight values in session.xml. Height is '{$height}', Weight is '{$weight}'.");
+    }
+} else {
+    die("Error: The session.xml file does not exist in the specified directory.");
+}
+
+
+
+
+?>
